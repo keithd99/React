7af989326044b342ce3e99c4662860a6f2a5e89f
Keith Doran <keith.doran@activeops.com>
2018-09-05 22:27:53 +0100

    Finished CSS Module

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -43,16 +43,8 @@ class App extends Component {
     };
 
     render() {
-        const style = {
-            backgroundColor: 'green',
-            color: 'white',
-            font: 'inherit',
-            border: '1px solid blue',
-            padding: '8px',
-            cursor: 'pointer'
-        };
-
         let persons = null;
+        let buttonClass = '';
         if (this.state.showPersons) {
             persons = (
                 <div>
@@ -66,11 +58,7 @@ class App extends Component {
                     })}
                 </div>
             );
-            style.backgroundColor = 'red';
-            style[':hover'] = {
-                backgroundColor: 'salmon',
-                color: 'black'
-            };
+            buttonClass = classes.Red;
         }
 
         const assignedClasses = [];
@@ -86,7 +74,7 @@ class App extends Component {
                 <h1>Hi I am a React App</h1>
                 <p className={assignedClasses.join(' ')}>This is really working</p>
                 <button
-                    style={style}
+                    className={buttonClass}
                     onClick={this.togglePersonsHandler}>Toggle Persons</button>
                 {persons}
             </div>
