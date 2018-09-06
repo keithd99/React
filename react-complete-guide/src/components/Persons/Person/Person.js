729be085eff132181291afcd302974d221271179
Keith Doran <keith.doran@activeops.com>
2018-09-06 21:57:23 +0100

    Restructure Solution - Section 7

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -3,6 +3,8 @@ import React from 'react';
 import classes from './Person.css';
 
 const person = (props) => {
+    const rnd = Math.random();
+    
     return (
         <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old</p>
