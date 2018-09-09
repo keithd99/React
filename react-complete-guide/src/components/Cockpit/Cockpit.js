7aea751b0c6e9d210c3cdec67cf6ee705ae93e93
Keith Doran <keith.doran@activeops.com>
2018-09-09 18:59:41 +0100

    Fixed Aux issue

--- react-complete-guide/src/components/Cockpit/Cockpit.js
+++ react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import classes from './Cockpit.css'
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/Aux2';
 
 const cockpit = (props) => {
     const assignedClasses = [];
--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import Aux from '../hoc/Aux';
+import Aux from '../hoc/Aux2';
 import withClass from '../hoc/withClass';
 
 export const AuthContext = React.createContext(false);
