257b7fd1e8227c6cc2c9dde6cc39b6f5b6529006
Keith Doran <keith.doran@activeops.com>
2018-09-10 11:51:39 +0100

    Before Checkout

--- burger-app/src/components/Burger/BuildControls/BuildControls.js
+++ burger-app/src/components/Burger/BuildControls/BuildControls.js
@@ -4,17 +4,23 @@ import classes from './BuildControls.css';
 import BuildControl from './BuildControl/BuildControl';
 
 const controls = [
-    {label: 'Salad', type: 'salad'},
-    {label: 'Bacon', type: 'bacon'},
-    {label: 'Cheese', type: 'cheese'},
-    {label: 'Meat', type: 'meat'}
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' }
 ]
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
+        <p>Current Price: <string>{props.price.toFixed(2)}</string></p>
         {controls.map(ctrl => (
-            <BuildControl key={ctrl.label} label={ctrl.label} />
-    ))}
+            <BuildControl
+                key={ctrl.label}
+                label={ctrl.label}
+                added={() => props.ingredientAdded(ctrl.type)}
+                removed={() => props.ingredientRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]} />
+        ))}
     </div>
 );
 export default buildControls;
