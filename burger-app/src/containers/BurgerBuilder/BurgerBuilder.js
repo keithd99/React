f1b0229b2725d84b0bfb6384d8064c37e1a99186
Keith Doran <keith.doran@activeops.com>
2018-09-10 11:20:58 +0100

    Before hooking up state to Burger Builder

--- burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Auxx';
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
     /* constructor(props){
@@ -10,14 +11,19 @@ class BurgerBuilder extends Component {
         };
     } */
     state = {
-
+        ingredients:{
+            salad:0,
+            bacon:0,
+            cheese:0,
+            meat:0
+        }
     }
 
     render(){
         return(
             <Aux>
-                <Burger />
-                <div>Burger Controls</div>
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls />
             </Aux>
         );
     }
